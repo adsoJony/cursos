e949e16ad98bd6296d4f6342a7ded5c97913e697
Jonathan Gomez <jonathan_agomezf@soy.sena.edu.co>
2025-05-12 16:45:12 -0500

    firs commit

---
+++ flexbox/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Flexbox</title>
+</head>
+
+<body>
+
+    <div class="title">
+        <h1>Flexbox</h1>
+    </div>
+    <div class="container">
+        <div class="title">
+            <h2>Flex-Wrap</h2>
+        </div>
+        <h4>Lineas (renglones)</h4>
+        <p>los elementos flexibles pueden disponerse en una sola linea o variass lineas (renglones) de acuerdo con la propiedad <strong>flex-wrap</strong> que controlan la dirección del eje secundario y la dirección en la que las nuevas lineas se apilan.</p>
+    </div>
+
+
+    <div class="container">
+        <div class="title">
+        <h3>No-Wrap</h3>
+        </div>
+        <div class="contenedor">
+            <div class="item">Item 1</div>
+            <div class="item">Item 1</div>
+            <div class="item">Item 2</div>
+            <div class="item">Item 4</div>
+        </div>
+    </div>
+<hr>
+    <div class="container">
+        <div class="title">
+        <h3>Wrap</h3>
+        </div>
+        <div class="contenedor wrap">
+            <div class="item">Item 1</div>
+            <div class="item">Item 2</div>
+            <div class="item">Item 3</div>
+            <div class="item">Item 4</div>
+        </div>
+    </div>
+
+    <hr>
+    <div class="container">
+        <div class="title">
+        <h3>Wrap-Reverse</h3>
+        </div>
+        <div class="contenedor wrap-reverse">
+            <div class="item">Item 1</div>
+            <div class="item">Item 2</div>
+            <div class="item">Item 3</div>
+            <div class="item">Item 4</div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cursos de Udemo</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+        <div class="title">
+        <h1>Cursos de Udemy Html</h1>
+        </div>
+        </div>
+    </div>
+    <hr>
+    <div class="container">
+        <div class="row">
+            <h2>Html</h2>
+        </div>
+        <div class="row">
+            <li>
+                <ul><a href="flexbox/">Flexbox</a></ul>
+            </li>
+        </div>
+    </div>
+
+    
+</body>
+</html>
